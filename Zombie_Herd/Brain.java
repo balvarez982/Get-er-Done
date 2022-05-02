bca92022031662b4fb5a229798d3b2de4c75b2fa
SMcCarthy0505 <smccarthy003@drury.edu>
2022-05-02 12:06:06 -0500

    Added Brain

---
+++ Zombie_Herd/Brain.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Brain here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Brain extends Actor
+{
+    /**
+     * Act - do whatever the Brain wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
