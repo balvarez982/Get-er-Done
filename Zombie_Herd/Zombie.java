b238c54466d33ba872f86ba686f3edc2be0503a5
nihil-dev <nihil.ctrl@gmail.com>
2022-05-06 20:55:45 -0700

    Fixed zombie AI, added state machine

--- Zombie_Herd/Zombie.java
+++ Zombie_Herd/Zombie.java
@@ -8,13 +8,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Zombie extends Actor
 {
-    private int test = 0;
-    private int wanderCheck;
-    private int timer2 = 150;
-    private int wanderTime = 150;
+    private int state = 0;
+    // idle = 0
+    // move = 1
     
+    final int speed = 2;  
+    final int maxDistance = 100; 
+    final int minPauseTime = 50;
+    final int maxPauseTime = 150;
+    
+    private int direction = 0;
+    private int targetDistance = 0;
+    private int pauseTimer = 0;
     
     private GreenfootImage[] images = new GreenfootImage[2];
+    
     public Zombie() {
         GreenfootImage image = getImage();
         image.scale(50,50);
@@ -23,53 +31,60 @@ public class Zombie extends Actor
         images[0] = image;
         images[1] = new GreenfootImage(images[0]);
         images[1].mirrorHorizontally();
+        
+        setState(state);
     }
     
-    private int timer = 1;
-    private int speed = 1;  
-    
-    private void movement(int check) {
-        timer--;
-        if (timer > 0) {
-            if(speed < 0) {
-               move(speed);
-               setImage(images[1]);
-            }
-            else {
-               move(speed);
-               setImage(images[0]);
-            }
-        }
-        if (timer == 0) {
-            speed = -2-Greenfoot.getRandomNumber(100); // max -2; min -31
-        }
-        if (timer < 0 && timer == speed) {
-            speed = 1-2*Greenfoot.getRandomNumber(2); // 1 or -1
-            timer = 2+Greenfoot.getRandomNumber(100); // min 2; max 31
+    private void movement() {
+        if (direction == 0) {
+            setRotation(0);
+            setImage(images[0]);
+            setLocation(getX()+speed, getY());
+            targetDistance -= speed;
+        } else if (direction == 1) {
+            setRotation(-90);
+            setImage(images[0]);
+            setLocation(getX(), getY()-speed);
+            targetDistance -= speed;
+        } else if (direction == 2) {
+            setRotation(0);
+            setImage(images[1]);
+            setLocation(getX()-speed, getY());
+            targetDistance -= speed;
+        } else if (direction == 3) {
+            setRotation(90);
+            setImage(images[0]);
+            setLocation(getX(), getY()+speed);
+            targetDistance -= speed;
         }
     }
     
     private void wander() {
-        //wanderTimer = (wanderTimer+1)%180;
-        //if(wanderTimer == 0) {
-        //}
-        wanderCheck = Greenfoot.getRandomNumber(1);
-        movement(wanderCheck);
+        if (state == 0) {
+            if (pauseTimer > 0) {
+                pauseTimer--;
+            } else if (pauseTimer == 0) {
+                setState(1);
+            }
+        } else if (state == 1) {
+            if (targetDistance > 0) {
+                movement();
+            } else if (targetDistance <= 0) {
+                setState(0);
+            }
         }
+    }
+    
+    private void setState(int newState) {
+        state = newState;
         
-        
-        /*
-        if(wanderTimer < 0) {
-            wanderTimer++;
+        if (newState == 0) {
+            pauseTimer = minPauseTime+Greenfoot.getRandomNumber(maxPauseTime-minPauseTime);
+        } else if (newState == 1) {
+            direction = Greenfoot.getRandomNumber(4);
+            targetDistance = Greenfoot.getRandomNumber(maxDistance);
         }
-        else if(wanderTimer >= 300) {
-            setRotation(Greenfoot.getRandomNumber(360));
-            move(5);
-            wanderTimer++;
-        }
-        */
-       
-       
+    }
         
     /**
      * Act - do whatever the Zombie wants to do. This method is called whenever
@@ -77,7 +92,6 @@ public class Zombie extends Actor
      */
     public void act()
     {
-        // Add your action code here.
         wander();
     }
 }
